7d3fd381cfb43fd1321bd6490adf3b4fbaefca2a
raultocantins <alexbraul.ar@gmail.com>
2020-04-30 19:10:37 -0300

    Alteração no design do component register e correção na responsividade do mesmo

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,65 +1,53 @@
 import React from 'react'
 import '../Register/Register.css'
-export default function () {
-    return (
 
-        <div className="box container bg-light mt-2">
-            <form action="/" method="POST">
-                <h1 className="text-center mt-3">Cadastro de Usuario</h1>
-                <div className="row d-flex justify-content-between mt-3">
-                    <input type="text" class="form-control col mr-1" placeholder="Nome*" aria-describedby="basic-addon1" />
-                    <input type="text" class="form-control col" placeholder="Sobrenome*" aria-describedby="basic-addon1" />
+export default class Register extends React.Component {
+
+    render() {
+        return (
+            <div className="container-fluid box d-flex mt-4 bg-danger">
+                <div className="row ">
+                    <div className="col-md-4 ">
+
+                        <p>inserir carousel de svgs sobre gerenciamento</p>
+                    </div>
+                    <div className="col-md-8 p-md-4 col-12 p-xs-1 divform mt-3 mb-3   ">
+                        <form action="/" method="POST">
+                            <h1 className="text-center ">Pessoa Fisica</h1>
+                            <p className="text-muted text-center">Cadastre seus dados </p>
+                            <div className="row d-flex ">
+                                <input type="text" class="form-control col-12  mt-1 " placeholder="Qual seu nome completo?" aria-describedby="basic-addon1" />
+                                <input type="email" class="form-control col-6 mt-4 " placeholder="Email*" aria-describedby="basic-addon1" />
+                                <input type="password" class="form-control col-3 mt-4  " placeholder="Senha*" aria-describedby="basic-addon1" />
+                                <input type="password" class="form-control col-3 mt-4  " placeholder="Confirmar Senha*" aria-describedby="basic-addon1" />
+                                <select class="custom-select col-6 mt-4  " id="inputGroupSelect01">
+                                    <option selected>Pergunta de Segurança</option>
+                                    <option value="1">Qual seu animal favorito?</option>
+                                    <option value="2">Qual esporte você mais gosta?</option>
+                                    <option value="3">Nome da(o) melhor professor(a)?</option>
+                                </select>
+                                <input type="text" class="form-control col-6 mt-4  " placeholder="Resposta*" aria-describedby="basic-addon1" />
+                                <select id="inputState" class="form-control col-4 mt-4 " >
+                                    <option selected>Estado</option>
+                                    <option>TO</option>
+                                    <option>PA</option>
+                                    <option>AM</option>
+                                    <option>MA</option>
+                                    <option>GO</option>
+                                    <option>CE</option>
+                                </select>
+                                <input type="text" placeholder="Cep" class="form-control col-4 mt-4  " id="inputZip" />
+                                <select id="inputState" class="form-control col-4 mt-4  ">
+                                    <option selected>Sexo</option>
+                                    <option>Masculino</option>
+                                    <option>Feminino</option>
+                                </select>
+                                <button type="button" class="btn btn-danger btn-md col-md-3 mt-5 ">Próximo</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div className="row d-flex justify-content-between mt-5">
-                    <input type="email" class="form-control col mr-1" placeholder="Email*" aria-describedby="basic-addon1" />
-                    <input type="password" class="form-control col mr-1" placeholder="Senha*" aria-describedby="basic-addon1" />
-                    <input type="password" class="form-control col" placeholder="Confirmar Senha*" aria-describedby="basic-addon1" />
-                </div>
-                <div className="row d-flex justify-content-between mt-5">
-                    <select class="custom-select col mr-2" id="inputGroupSelect01">
-                        <option selected>Pergunta de Segurança</option>
-                        <option value="1">Qual seu animal favorito?</option>
-                        <option value="2">Qual esporte você mais gosta?</option>
-                        <option value="3">Nome da(o) melhor professor(a)?</option>
-                    </select>
-                    <input type="text" class="form-control col" placeholder="Resposta*" aria-describedby="basic-addon1" />
-                </div>
-
-                <div className="row d-flex mt-5 justify-content-around">
-                    <select id="inputState" class="form-control col-3 mr-4 ">
-                        <option selected>Estado</option>
-                        <option>TO</option>
-                        <option>PA</option>
-                        <option>AM</option>
-                        <option>MA</option>
-                        <option>GO</option>
-                        <option>CE</option>
-                    </select>
-
-                    <input type="text" placeholder="Cep" class="form-control col-5 mr-4 " id="inputZip" />
-
-                    <select id="inputState" class="form-control col-3 ">
-                        <option selected>Sexo</option>
-                        <option>Masculino</option>
-                        <option>Feminino</option>
-                    </select>
-                </div>
-
-
-
-      
-
-                <div className="row mt-5 d-flex">
-                    <button type="submit" class="btn btn-danger btn-lg col">Cadastrar</button>
-                </div>
-
-
-
-
-            </form>
-        </div>
-
-
-
-    );
+            </div>
+        );
+    }
 }
